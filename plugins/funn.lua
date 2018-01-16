5927382149a2db51cdd1d8fb71f1e6db3d6e0c27
mehdi poinshtan <33298289+poinshtan@users.noreply.github.com>
2018-01-16 05:31:54 -0800

    Update funn.lua

--- plugins/funn.lua
+++ plugins/funn.lua
@@ -217,7 +217,7 @@ local Clang = redis:get(Chash)
 local url , res = http.request('http://irapi.ir/time/')
 if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
-local text = '*`ساعت به وقت ایران`:* _'..jdat.FAtime..'_\n*`تاریخ ایران`:* _'..jdat.FAdate..'_\n------------\n*En Time:* _'..jdat.ENtime..'_\n *En Data:* _'..jdat.ENdate.. '_\n'
+local text = '*ساعت به وقت ایران:* _'..jdat.FAtime..'_\n*تاریخ ایران:* _'..jdat.FAdate..'_\n------------\n*En Time:* _'..jdat.ENtime..'_\n *En Data:* _'..jdat.ENdate.. '_\n'
   tdcli.sendMessage(msg.chat_id_, 0, 1, text, 1, 'md')
 	end
 --------------------------------
