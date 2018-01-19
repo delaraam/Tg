48a8f4c1a652a6f898091d1598ecb59bd14ae23d
mehdi poinshtan <33298289+poinshtan@users.noreply.github.com>
2018-01-19 08:12:22 -0800

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -1,5 +1,5 @@
 --Begin Tools.lua :)
-local SUDO = ایدی عددی مدیر -- put Your ID here! <===
+local SUDO = ownerID -- put Your ID here! <===
 function exi_files(cpath)
     local files = {}
     local pth = cpath
